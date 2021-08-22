1fc83c9867486dde42dc1b52112fc0d6d654caec
Carlosdc206 <89317743+Carlosdc206@users.noreply.github.com>
2021-08-21 22:12:11 -0500

    Add files via upload

---
+++ Sum #10.cpp
@@ -0,0 +1,18 @@
+
+int main(){
+	int num=15,numA=0
+		int num, nombre, res;
+		printf("Introduzca numero: ");
+		scanf("%d . &num");
+		(num=15){
+			printf("Quince")
+		}else{
+			if(num=15){
+			res=pow(num^3)
+			printf(" El cubo de %d es: %d\n",num,res);
+		}else{
+			printf("El numero positivo\n")
+		}
+	}
+	return 0;
+}
